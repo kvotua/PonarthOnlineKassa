8b38bc167a4cc5fd5c9c17d995295fd030c286b9
ErionNarrator <nikitapavliy@gmail.com>
2025-07-17 14:40:03 +0300

    refactor:обновление программы лояльности, отображение информации о пользователе после входа или регистрации

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.routes import cart, products, users, auth, verify, good
+from app.api.routes import cart, products, users, auth, verify
 
 api_router = APIRouter(prefix='/api/v1')
 
@@ -10,4 +10,3 @@ api_router.include_router(verify.router)
 # api_router.include_router(products.router)
 # api_router.include_router(cart.router)
 
-api_router.include_router(good.router)
